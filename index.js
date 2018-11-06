97722976f41f3528cdf287472d2b50836440cea8
Théodore Orfèvres <theodore.orfevres@ynov.com>
2018-11-06 17:32:28 +0100

    mongoose

--- index.js
+++ index.js
@@ -3,37 +3,29 @@ let config = require('config');
 let express = require('express');
 let jwt = require('jsonwebtoken');
 let bodyParser = require('body-parser');
-let MongoClient = require('mongodb').MongoClient;
-let Db = require('./db');
+let mongoose = require('mongoose');
 
-let client = new MongoClient(config.get('mongodb.url'), { useNewUrlParser: true });
-let db = new Db(client, config.get('mongodb.db_name'));
+mongoose.connect(config.get('mongoose.url'), { useNewUrlParser: true });
 let app = express();
-let models = [];
+let models = [
+    mongoose.model('user', new mongoose.Schema({
+        name: String,
+        password: String
+    })),
+    mongoose.model('item', new mongoose.Schema({
+        label: String,
+        image: String,
+        description: String
+    })),
+    mongoose.model('list', new mongoose.Schema({
+        name: String,
+        user: String,
+        items: [String]
+    }))
+];
 console.clear();
 
 let formatLog = (str) => (new Date()).toDateString() + "::" + str + "\n";
-
-let checkDataConstraints = (model, values) => {
-    return new Promise((resolve, reject) => {
-        let obj = new model();
-        let missingKeys = [];
-
-        let additionalKeys = Object.keys(values);
-        Object.keys(obj).forEach(k => {
-            additionalKeys = additionalKeys.filter(e => e !== k.split('_')[1]);
-        });
-
-        Object.keys(obj).forEach(k => {
-            let key = k.split('_')[1];
-            if (!values.hasOwnProperty(key)) missingKeys.push(key);
-        });
-
-        if (missingKeys.length > 0) reject("Missing keys: " + missingKeys);
-        if (additionalKeys.length > 0) reject("Additional keys: " + additionalKeys);
-        resolve();
-    });
-};
 
 app.use(bodyParser.json());
 
@@ -42,12 +34,12 @@ app.all('/', (req, res) => {
     if (value) {
         let token = jwt.sign({user: value, version: '1.0.0'}, config.get('server.secret'));
         res.send({
-            paths: [...models].map(e => [ '/' + e.toLowerCase(), [ 'GET', 'POST', 'PUT', 'DELETE' ]]),
+            paths: [...models].map(e => ['/' + e.collection.name.toLowerCase(), ['GET', 'POST', 'PUT', 'DELETE']]),
             token: token
         });
     } else {
         res.send({
-            paths: [...models].map(e => [ '/' + e.toLowerCase(), [ 'GET', 'POST', 'PUT', 'DELETE' ]]),
+            paths: [...models].map(e => ['/' + e.collection.name.toLowerCase(), ['GET', 'POST', 'PUT', 'DELETE']]),
             token: 'You must provide a user as GET param.'
         });
     }
@@ -63,72 +55,58 @@ app.use(
         }
     },
     (req, res, next) => {
-        fs.writeFileSync('./logs/request.log', formatLog(req.method + " " + req.originalUrl), { flag: 'a' });
+        fs.writeFileSync('./logs/request.log', formatLog(req.method + " " + req.originalUrl), {flag: 'a'});
         next();
+    });
+
+models.forEach(m => {
+    // READ
+    app.get('/' + m.collection.name.toLowerCase(), (req, res, next) => {
+        m.find({}, (err, data) => {
+            if (err) next(err);
+            res.send(data);
+        })
+    });
+    app.get('/' + m.collection.name.toLowerCase() + '/:id', (req, res, next) => {
+        m.find({ id: req.params.id }, (err, data) => {
+            if (err) next(err);
+            res.send(data);
+        })
+    });
+    // CREATE
+    app.post('/' + m.collection.name.toLowerCase(), (req, res, next) => {
+        m.create(req.body, (err, data) => {
+            if (err) next(err);
+            res.send(data);
+        })
+    });
+    // UPDATE
+    app.put('/' + m.collection.name.toLowerCase(), (req, res, next) => {
+        m.findByIdAndUpdate(req.body.id, req.body, (err, data) => {
+            if (err) next(err);
+            res.send(data);
+        })
+    });
+    // DELETE
+    app.delete('/' + m.collection.name.toLowerCase(), (req, res, next) => {
+        m.findByIdAndRemove(req.body.id, (err, data) => {
+            if (err) next(err);
+            res.send(data);
+        })
+    });
 });
 
-fs.readdir('./models', (err, files) => {
-    files.forEach(file => {
-        let str = file.split(".js")[0];
-        models.push(str.charAt(0).toUpperCase() + str.slice(1));
-    });
+app.use((err, req, res, next) => {
+    if (err) {
+        fs.writeFileSync('./logs/error.log', formatLog(JSON.stringify(err)), {flag: 'a'});
+        res.status(500).send("Your request " + req.method + " hasn't been processed. " + err);
+    }
+    next();
+});
 
-    models.forEach(m => {
-        let model = require('./models/' + m);
-
-        // READ
-        app.get('/' + m.toLowerCase(), (req, res) => {
-            db.readData(m)
-                .then((data) => res.send(data));
-        });
-        app.get('/' + m.toLowerCase() + '/:id', (req, res) => {
-            db.readData(m)
-                .then((data) => res.send(data.filter(e => e.id === req.params.id)));
-        });
-        // CREATE
-        app.post('/' + m.toLowerCase(), (req, res, next) => {
-            let values = req.body;
-            checkDataConstraints(model, values)
-                .then(() => {
-                    db.writeData(m, values);
-                    res.send(m + ' created.');
-                })
-                .catch((err) => next(err));
-        });
-        // UPDATE
-        app.put('/' + m.toLowerCase(), (req, res, next) => {
-            let values = req.body;
-            checkDataConstraints(model, values)
-                .then(() => {
-                    db.updateData(m, values);
-                    res.send(m + ' updated.');
-                })
-                .catch((err) => next(err));
-        });
-        // DELETE
-        app.delete('/' + m.toLowerCase(), (req, res, next) => {
-            let values = req.body;
-            checkDataConstraints(model, values)
-                .then(() => {
-                    db.deleteData(m, values);
-                    res.send(m + ' deleted.');
-                })
-                .catch((err) => next(err));
-        });
-    });
-
-    app.use((err, req, res, next) => {
-        if (err) {
-            fs.writeFileSync('./logs/error.log', formatLog(JSON.stringify(err)), { flag: 'a' });
-            res.status(500).send("Your request hasn't been processed. " + err);
-        }
-        next();
-    });
-
-    app.listen(config.get('server.port'), () => {
-        console.log(
-            'APP READY!:\n',
-            '• Listening on localhost:' + config.get('server.port') + '.\n'
-        );
-    });
+app.listen(config.get('server.port'), () => {
+    console.log(
+        'APP READY!:\n',
+        '• Listening on localhost:' + config.get('server.port') + '.\n'
+    );
 });
